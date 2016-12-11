6dc9ca11b2f0bf5e701149c55a248546551bbce0
Richard Rosko <rosko.richard@gmail.com>
2016-12-11 19:48:25 +0100

    initial commit

---
+++ framework/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php insert("partials/head") ?>
+</head>
+<body>
+	<div class="container">
+		<?php insert("partials/flash_message") ?>
+
+		<div class="row">
+			<div class="col s12 m12 l12">
+				<div class="card">
+					<div class="card-content">
+						<h3>Welcome to phpM</h3>
+						<p>Take a look around</p>
+						<div class="card-action">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="<?php asset("js/app.min.js") ?>" type="text/javascript"></script>
+</body>
+</html>
