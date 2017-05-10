1da3db2c61c9c8d9fa11042647c8ad98eec834fe
Richard Rosko <rosko.richard@gmail.com>
2017-05-10 17:54:26 +0200

    php max execution script upped to 5 miutes

--- config/system.php
+++ config/system.php
@@ -1,4 +1,6 @@
 <?php
+// set the maximum execution time of script to 5 minutes (due to lengthy archiving processes handled by crontab)
+ini_set('max_execution_time', 300);
 
 // configure the error reporting
 error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
