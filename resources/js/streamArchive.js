93b98c1650fff01731eaadc041b193a538742a81
Richard Rosko <rosko.richard@gmail.com>
2017-05-10 18:08:38 +0200

    archive hardcoded

--- resources/js/streamArchive.js
+++ resources/js/streamArchive.js
@@ -6,10 +6,11 @@ var getCameraId = function(camera_id) {
 
 $(document).ready(function() {
 	var timestamp = 1494431161;
+
 	setInterval(function() {
 		$("#archive_camera_stream").attr(
 			"src",
-			"http://localhost:4000" + "/" + "public" + "/" + "streamArchive" + "/" + cameraId + "/" + timestamp + ".png");
+			"http://localhost:4000" + "/" + "public" + "/" + "streamArchive" + "/" + 2 + "/" + timestamp + ".png");
 		++timestamp;
 	}, 1000);
 });
